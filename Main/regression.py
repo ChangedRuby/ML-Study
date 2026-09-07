e0b5a6b1e406197b7c021473f568b02dde2a39ac
ChangedRuby <gugasboy7@gmail.com>
2026-09-07 12:27:04 -0300

    finish poda

--- Main/regression.py
+++ Main/regression.py
@@ -23,8 +23,8 @@ B = np.linalg.pinv(X.T@X)@X.T@y
 
 x_axis = np.linspace(data[0,0], data[-1,0], 100)
 x_axis = x_axis.reshape(len(x_axis), 1)
-X_new = np.concatenate((np.ones((len(x_axis), 1)), x_axis),axis=1)
-Y_pred = X_new@B
+X_testing = np.concatenate((np.ones((len(x_axis), 1)), x_axis), axis=1)
+Y_pred = X_testing @ B
 
 plt.plot(x_axis, Y_pred)
 # plt.show()
@@ -47,8 +47,8 @@ B = np.linalg.pinv((X.T@X) + (lamb*I))@X.T@y
 x_axis = np.linspace(data[0,0], data[-1,0], 100)
 x_axis = x_axis.reshape(len(x_axis), 1)
 x_axis_normalized = (x_axis - x.mean()) / x.std()
-X_new = np.concatenate((np.ones((len(x_axis_normalized), 1)), x_axis_normalized),axis=1)
-Y_pred = X_new@B
+X_testing = np.concatenate((np.ones((len(x_axis_normalized), 1)), x_axis_normalized), axis=1)
+Y_pred = X_testing @ B
 
 plt.plot(x_axis, Y_pred)
 
@@ -66,8 +66,8 @@ B = np.linalg.pinv((X.T@X) + (lamb*I))@X.T@y
 
 x_axis = np.linspace(data[0,0], data[-1,0], 100)
 x_axis = x_axis.reshape(len(x_axis), 1)
-X_new = np.concatenate((np.ones((len(x_axis), 1)), x_axis),axis=1)
-Y_pred = X_new@B
+X_testing = np.concatenate((np.ones((len(x_axis), 1)), x_axis), axis=1)
+Y_pred = X_testing @ B
 
 plt.plot(x_axis, Y_pred)
 
@@ -87,20 +87,83 @@ for i, n in enumerate(range(2, p+1)):
 
 I = np.eye(X.shape[1])
 I[0,0] = 0
-lamb = 100
+lamb = 1
 B = np.linalg.pinv((X.T@X) + (lamb*I))@X.T@y
 
 x_axis = np.linspace(data[0,0], data[-1,0], 100)
 x_axis = x_axis.reshape(len(x_axis), 1)
 x_axis_normalized = (x_axis - x.mean()) / x.std()
-X_new = np.concatenate((np.ones((len(x_axis_normalized), 1)), x_axis_normalized),axis=1)
+X_testing = np.concatenate((np.ones((len(x_axis_normalized), 1)), x_axis_normalized), axis=1)
 
 for i, n in enumerate(range(2, p+1)):
-    X_new = np.concatenate((X_new, x_axis_normalized**n), axis=1)
+    X_testing = np.concatenate((X_testing, x_axis_normalized ** n), axis=1)
 
-Y_pred = X_new@B
+Y_pred = X_testing @ B
 
 plt.plot(x_axis, Y_pred)
+
+#4 Estrategia de poda
+
+plt.figure(4)
+plt.xlabel("Ano")
+plt.ylabel("PIB")
+
+indicies = np.random.permutation(len(x))
+x_shuffled = x[indicies]
+y_shuffled = y[indicies]
+x_training = x_shuffled[:int(.8*len(x_shuffled))]
+x_testing = x_shuffled[int(.8*len(x_shuffled)):]
+x_training_normalized = (x_training - x_training.mean()) / x_training.std()
+x_testing_normalized = (x_testing - x_training.mean()) / x_training.std()
+y_training = y_shuffled[:int(.8*len(y_shuffled))]
+y_testing = y_shuffled[int(.8*len(y_shuffled)):]
+
+plt.scatter(x_training, y_training, color="blue")
+
+X = np.concatenate((np.ones((len(x_training_normalized), 1)), x_training_normalized), axis=1)
+p = 10
+
+for i, n in enumerate(range(2, p+1)):
+    X = np.concatenate((X, x_training_normalized**n), axis=1)
+
+I = np.eye(X.shape[1])
+I[0,0] = 0
+lamb = 1
+B = np.linalg.pinv((X.T@X) + (lamb*I))@X.T@y_training
+
+####### TESTA COM OS DADOS DE TESTE ########
+X_testing = np.concatenate((np.ones((len(x_testing_normalized), 1)), x_testing_normalized), axis=1)
+
+for i, n in enumerate(range(2, p+1)):
+    X_testing = np.concatenate((X_testing, x_testing_normalized**n), axis=1)
+
+Y_pred = X_testing@B
+
+R_squared = 1 - (np.sum((y_testing - Y_pred)**2) / np.sum((y_testing - np.mean(y_testing))**2))
+print(R_squared)
+###################
+
+####### CRIA O PLOT COM 100 DADOS COM LINSPACE ########
+# função antiga quando o plot era os próprios dados de teste
+# ordered_indicies = np.argsort(x_testing, axis=0).flatten()
+# criar o plot dos dados de teste
+# plt.plot(x_testing[ordered_indicies], Y_pred[ordered_indicies])
+
+x_plot = np.linspace(np.union1d(x_training, x_testing).min(), np.union1d(x_training, x_testing).max(), 100)
+x_plot = x_plot.reshape(len(x_plot), 1)
+x_plot_normalized = (x_plot - x_training.mean()) / x_training.std()
+X_plot = np.concatenate((np.ones((len(x_plot_normalized), 1)), x_plot_normalized), axis=1)
+
+for i, n in enumerate(range(2, p+1)):
+    X_plot = np.concatenate((X_plot, x_plot_normalized**n), axis=1)
+
+Y_pred_plot = X_plot@B
+###################
+
+plt.title(f"R2 = {R_squared:.8f}")
+plt.scatter(x_testing, y_testing, color="red")
+plt.scatter(x_testing, Y_pred, marker="x", color="orange")
+plt.plot(x_plot, Y_pred_plot)
 plt.show()
 
 print()
